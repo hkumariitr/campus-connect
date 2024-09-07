c28a3b4434bab06f538810886d29c7c8079d62fc
Harshit <harshitkumarppnews9@gmail.com>
2024-09-07 07:21:42 +0530

    deployment fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,12 +7,10 @@ export default defineConfig({
     outDir: 'dist',
   },
   server: {
-    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false,
       },
     },
   },
